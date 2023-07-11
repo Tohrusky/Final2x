1d38f608ab94cc847edf6ac81f95570d55601b5f
tohru <65994850+Tohrusky@users.noreply.github.com>
2023-07-11 15:04:53 +0800

    update CI test

--- test/utils/getFinal2xconfig.test.ts
+++ test/utils/getFinal2xconfig.test.ts
@@ -15,19 +15,19 @@ describe('getFinal2xconfig', () => {
 
   it('test_getFinal2xconfig', () => {
     expect(getFinal2xconfig()).toEqual(
-      '{"gpuid":0,"inputpath":[],"model":"RealCUGAN-pro","modelscale":2,"modelnoise":-1,"outputpath":"","targetscale":0,"tta":false}'
+      '{"gpuid":0,"inputpath":[],"model":"RealESRGAN","modelscale":2,"modelnoise":-1,"outputpath":"","targetscale":0,"tta":false}'
     )
     ioPath.setoutputpath('/test')
     expect(getFinal2xconfig()).toEqual(
-      '{"gpuid":0,"inputpath":[],"model":"RealCUGAN-pro","modelscale":2,"modelnoise":-1,"outputpath":"/test","targetscale":0,"tta":false}'
+      '{"gpuid":0,"inputpath":[],"model":"RealESRGAN","modelscale":2,"modelnoise":-1,"outputpath":"/test","targetscale":0,"tta":false}'
     )
     ioPath.add('114514', '/test1')
     expect(getFinal2xconfig()).toEqual(
-      '{"gpuid":0,"inputpath":["/test1"],"model":"RealCUGAN-pro","modelscale":2,"modelnoise":-1,"outputpath":"/test","targetscale":0,"tta":false}'
+      '{"gpuid":0,"inputpath":["/test1"],"model":"RealESRGAN","modelscale":2,"modelnoise":-1,"outputpath":"/test","targetscale":0,"tta":false}'
     )
     ioPath.add('1919810', '/test2')
     expect(getFinal2xconfig()).toEqual(
-      '{"gpuid":0,"inputpath":["/test1","/test2"],"model":"RealCUGAN-pro","modelscale":2,"modelnoise":-1,"outputpath":"/test","targetscale":0,"tta":false}'
+      '{"gpuid":0,"inputpath":["/test1","/test2"],"model":"RealESRGAN","modelscale":2,"modelnoise":-1,"outputpath":"/test","targetscale":0,"tta":false}'
     )
   })
 })
