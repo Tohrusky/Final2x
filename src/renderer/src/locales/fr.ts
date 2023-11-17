b3f2356dc81263ff831302a795eee22239178c3b
mrintrepide <40322493+mrintrepide@users.noreply.github.com>
2023-11-17 18:16:15 +0100

    French locale

---
+++ src/renderer/src/locales/fr.ts
@@ -0,0 +1,44 @@
+export const fr = {
+  MyExternalLink: {
+    text0: 'Numéro de groupe QQ :',
+    text1: 'Copié dans le presse-papiers',
+    text2: 'Échec de la copie du numéro de groupe QQ'
+  },
+  MyProgress: {
+    text0: 'Traitement commencé',
+    text1: 'Traitement en cours',
+    text2: 'Veuillez ajouter une image',
+    text3: 'La liste d\'images est vide',
+    text4: 'Traitement terminé',
+    text5: 'Échec du traitement, passage à la suite',
+    text6: 'DÉMARRER',
+    text7: 'ARRÊTER',
+    text8: 'JOURNAL',
+    text9: 'Échec du traitement',
+    text10: 'Veuillez cliquer sur le journal pour voir le message d\'erreur'
+  },
+  Final2xHome: {
+    text0: 'Suppression réussie',
+    text1: 'Cliquez ou faites glisser les images ou dossiers ici pour les téléverser'
+  },
+  Final2xSettings: {
+    text0: 'Lorsque l\'ordre d\'affichage GPU est incorrect pour le périphérique chargé, choisissez-en un autre',
+    text1: 'RealCUGAN est un modèle de super-résolution d\'images anime entraîné sur des millions de données anime, prenant en charge une restauration conservatrice',
+    text2: 'RealESRGAN est un modèle de super-résolution pour la restauration générale d\'images',
+    text3: 'Waifu2x est un modèle de super-résolution d\'images anime qui prend également en charge les photos',
+    text4: 'Super-Resolution Network for Multiple Degradations',
+    text5: 'Facteur d\'échelle du modèle',
+    text6: 'Puissance de débruitage du modèle, -1 pour une restauration conservatrice',
+    text7: 'Cela augmentera le temps de traitement mais améliorera la qualité de l\'image',
+    text8: 'Facteur d\'échelle personnalisé, agrandissez plusieurs fois en utilisant le modèle sélectionné',
+    text9: 'Définir le chemin de sortie',
+    text10: 'Périphérique',
+    text11: 'Modèle',
+    text12: 'Échelle du modèle',
+    text13: 'Débruitage',
+    text14: 'TTA',
+    text15: 'Échelle personnalisée',
+    text16: 'Par défaut',
+    text17: 'Dossier de sortie'
+  }
+}
